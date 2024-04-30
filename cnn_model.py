37df6c051fc4b5f85ea73ee7d96a650458a88440
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-04-29 17:42:27 -0700

    Update cnn_model.py

--- cnn_model.py
+++ cnn_model.py
@@ -7,31 +7,42 @@ import os
 import nibabel as nib
 
 # loading in data
+# epoch - one complete pass through the ENTIRE training dataset during the training of the model
+# batch: portion of an epoch
+# during one epoch, the model will see each training model once and will update its parameters based on the observed data
 class DataGenerator(Sequence): # defines custom class that inherits from Keras Sequence class. 
     # Instead of loading the entire dataset to train the model, the generator loads and processes a batch of data
     # Feeds each batch to the model for training and moves on
     # Optimal for handling large amounts of data
     def __init__(self, image_paths, labels, batch_size=32, shuffle=True):
+        # initializes data generator with image paths, corresponding labels, batch size, and shuffle option
         self.image_paths = image_paths # stores list of file paths to images in the dataset
         self.labels = labels # stores corresponding labels for each image of the dataset (like key or id)
         self.batch_size = batch_size # specifies the size of each batch that will be sent to the model
-        self.shuffle = shuffle # 
-        self.indexes = np.arange(len(image_paths))
-        self.on_epoch_end()
-
+        self.shuffle = shuffle # indicates whether shuffle should be applied between epochs 
+        # important to prevent the model from memorizing the order of the samples, improving generalization
+        self.indexes = np.arange(len(image_paths)) # array of indexes representing the positions of the samples in the dataset
+        self.on_epoch_end() # called to shuffle the dataset each epoch
 
     def __len__(self):
-        return len(self.image_paths) // self.batch_size
+        # returns number of batches per epoch 
+        return len(self.image_paths) // self.batch_size 
 
     def __getitem__(self, index):
+        # generates one batch of data
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        # calculates indexes of the data samples to include in current batch 
         batch_image_paths = [self.image_paths[i] for i in indexes]
+        # retrieves the image paths corresponding to selected indexes 
         batch_labels = [self.labels[i] for i in indexes]
+        # retrieves labels corresponding to selected indexes 
 
         #preprocess batch of images
         preprocessed_images = []
         for image_path in batch_image_paths:
+            # code inside loop will process each image in a batch 
             image_path = self.load_nifti_image(image_data)
+            # 
             preprocessed_image = self.preprocess_image(image_data)
             preprocessed_images.append(preprocessed_image)
 
