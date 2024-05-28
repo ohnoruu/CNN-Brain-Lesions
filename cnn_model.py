cfc88e49baf221dd87b34d0dec54a8e91fa4850d
ohnoruu <143292762+ohnoruu@users.noreply.github.com>
2024-05-27 22:12:04 -0700

    untested classification task done
    modify classification later
    to do: research visualization task

--- cnn_model.py
+++ cnn_model.py
@@ -53,9 +53,9 @@ class DataGenerator(Sequence): # defines custom class that inherits from Keras S
         preprocessed_images = []
         for image_path in batch_image_paths:
             # code inside loop will process each image in a batch 
-            image_path = self.load_nifti_image(image_data)
+            image_path = self.load_nifti_image(image_path)
             # load image using load_nifti_image function, which retrieves image file and converts into array
-            preprocessed_image = self.preprocess_image(image_data)
+            preprocessed_image = self.preprocess_image(image_path)
             # preprocess image using preprocess_image function, which changes the image format to fit 3D ResNet
             preprocessed_images.append(preprocessed_image)
             # appends preprocessed image to a list of preprocessed images (defined above)
@@ -76,10 +76,10 @@ class DataGenerator(Sequence): # defines custom class that inherits from Keras S
         # get image as data array (records dimensions of the 3D nifti image)
         return image_data
 
-    def get_image_dimensions(self, image_data):
-        depth, height, width, channels = image_data.shape
-        print
-        return (depth, height, width, channels)
+    def preprocess_image(self, image):
+        # normalization
+        image_data = (image_data - np.min(image_data)) / (np.max(image_data) - np.min(image_data))
+        return image_data
 """
 Functions in tensorflow.keras
 Input - defines input layer
@@ -87,24 +87,56 @@ Conv3D - creates 3D Convolutional layer
 Model - creates neural network, defines architecture by specifying input and output layers of the model
 """
 
-def resnet_3d(input_shape):
+def classification(input_shape):
     # define 3D ResNet model using Keras
     inputs = Input(shape=input_shape)
     # creates an input layer for the model with the specified input shape
     # defines depth, height, width, and channels (colors)
     
-    outputs = Conv3D(1, kernel_size=(3,3,3), activation='sigmoid', padding='same')(inputs)
-    # creates a 3D Convolutional Layer
-    # 1 - number of filters (output channels) 
+    # for each convolutional layer, each step will be applied using var 'x'
+    x = Conv3D(32, kernel_size=(3,3,3), activation='sigmoid', padding='same')(inputs)
+    # creates a 3D Convolutional Layer on the input data
+    # 32 - number of filters (output channels) 
     # kernel-size=(3,3,3) - defines the size of a 3D convolutional kernel: a small matrix that is convolved with an input image, used to extract features
     # activation = 'sigmoid' - specifies activation function of the layer. sigmoid maps input values from a range between 1 and 0 (used for classification)
     # padding = 'same' - pads the input such as the output has the same dimensions as the input
-    
+    x = BatchNormalization()(x)
+    # normalizes the activations of previous layer
+    x = Activation('relu')(x)
+    # applies non-linear activation function
+    # helps in learning complex patterns in the dataset
+    x = MaxPooling3D(pool_size=(2,2,2))(x)
+    # reduces spatial dimensions of data, with a maximum pool size of 2x2x2
+    # helps to downsample the data and reduce computational load
+
+    x = Conv3D(64, kernel_size=(3,3,3), padding='same')(x)
+    # performs another 3D convolution, now with 64 filters alongside with normalization and relu activation
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling3D(pool_size=(2,2,2))(x)
+
+    feature_maps = x
+    # extract features from the data to use for visualization task
+
+    x = GlobalAveragePooling3D()(x)
+    # reduces the dimensions of the data by taking avg of all elements in each feature map
+    # data is reduced to a 1D vector --> passed to dense layer for classification
+    outputs = Dense(1, activation='sigmoid')(x)
+    # performs final classification using sigmoid function (value between 0 and 1)
+
     model = Model(inputs=inputs, outputs=outputs)
     # model creation
     # input - specifies input layer
     # output - specifies output layer
     return model
 
-#create 3D ResNet model
-#model = resnet_3d(input_shape)
+def visualization(image_data, feature_maps, threshold):
+    # apply threshold to feature maps to identify stroke areas
+    stroke_map = feature_maps > threshold
+
+    # segmentation/post-processing (note to do research on visualization later...)
+
+    highlighted_image = np.copy(image_data)
+    highlighted_image[stroke_map] = 255 # highlight stroke areas with white color
+
+    return highlighted_image
