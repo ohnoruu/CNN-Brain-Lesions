af3432bea9af226e8fc2cdcdc35d8e6b5568d510
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-09-05 12:43:24 -0700

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,8 +1,11 @@
 import numpy as np 
 import nibabel as nib
 import scipy.ndimage
+import os 
 
 from image_paths import image_paths
+from labels import labels
+from testing_paths import testing_paths
 
 def resize_image(image, target_size):
     if image.shape != target_size:
@@ -14,7 +17,8 @@ def resize_image(image, target_size):
     else:
         print("Image already has the target size (224, 224, 26, 1)")
         return image
-    
+
+"""
 if __name__ == '__main__':
     nifti_file_path = image_paths[0] # sub-1 (exception)
     # Most common dimensions: 224, 224, 26,1 (TRACE files)
@@ -24,5 +28,13 @@ if __name__ == '__main__':
     resized_image = resize_image(image_data, target_size)
     print(f'Original shape: {image_data.shape}')
     print(f'Resized shape: {resized_image.shape}')
-    
 # first test successful
+"""
+target_size = [224, 224, 26, 1]
+# MRI training images
+for image in image_paths:
+    if image.shape != target_size: 
+        nifti_img = nib.load(image)
+        image_data = nifti_img.get_fdata()
+        resized_image = resize_image(image_data, target_size)
+        # convert back to numpy array 
