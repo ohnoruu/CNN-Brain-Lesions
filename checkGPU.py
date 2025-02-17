f2233bc00da55a35a7fcd6cfa5cb3d5405945a85
ohnoruu <143292762+ohnoruu@users.noreply.github.com>
2025-02-17 12:07:37 -0800

    GPU Check

--- checkGPU.py
+++ checkGPU.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
 import os
+print("Tensorflow Version: ", tf.__version__)
 os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
 print("Num GPUs Available: ", tf.config.experimental.list_physical_devices('GPU'))
