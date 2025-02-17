54cf49b50a7a1040f10ff690437ded2aab77a313
ohnoruu <143292762+ohnoruu@users.noreply.github.com>
2025-02-17 13:06:15 -0800

    Working GPU configuration
    verified using checkGPU.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # imports
 import os
+# configure environment variables for GPU
+os.environ['CUDA_PATH'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2'
+os.environ['CUDA_PATH_V11_2'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2'
+os.environ['PATH'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\bin;' + os.environ['PATH']
+os.environ['PATH'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\libnvvp;' + os.environ['PATH']
+os.environ['PATH'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\include;' + os.environ['PATH']
+os.environ['PATH'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\extras\CUPTI\lib64;' + os.environ['PATH']
+os.environ['PATH'] = r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\lib\x64;' + os.environ['PATH']
 os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from pymongo import MongoClient
