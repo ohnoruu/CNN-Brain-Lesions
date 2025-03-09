08ed3338d0646fa36c3229aa351f72207bbf3088
ohnoruu <143292762+ohnoruu@users.noreply.github.com>
2025-03-09 15:42:58 -0700

    clean up code

--- plotmodel.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.utils import plot_model 
-from components.modelfunctions import segmentation
-
-model = segmentation((224, 224, 26, 1))
-plot_model(model, to_file='model.png', show_shapes=True, rankdir="LR")
